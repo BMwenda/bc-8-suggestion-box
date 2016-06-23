015cd796b0586b8f8b361e4537e499ac4f048c7c
Boniface Mwenda <bonny90.m@gmail.com>
2016-06-23 15:38:30 +0300

    Added methods for database queries

--- models.py
+++ models.py
@@ -3,35 +3,34 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from wtforms import Form, StringField, PasswordField, validators
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
-#Migrate db to flask
+# Migrate db to flask
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
 class User(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120), unique=True)
-    comment = db.relationship('Comment', backref='user',
-                              lazy='dynamic')
-    suggestion = db.relationship('Suggestion', backref='user',
-                                 lazy='dynamic')
 
-    def __init__(self, username, email, password, comment, suggestion):
+    def encrypt_password(self, password_hash):
+        self.password = generate_password_hash(password_hash)
+
+    def decrypt_password(self, password_hash):
+        return check_password_hash(self.password, password_hash)
+
+    def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = password
-        self.comment = comment
-        self.suggetion = suggestion
 
     def __repr__(self):
         return 'User {0}'.format(self.username)
@@ -42,18 +41,29 @@ class Suggestion(db.Model):
     title = db.Column(db.String(80))
     description = db.Column(db.String(140))
     posted_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    suggestion = db.relationship('User', backref=db.backref('user',
+                                 lazy='dynamic'))
+    votes = db.Column(db.Integer)
+    flags = db.Column(db.Integer)
 
-    def __init__(self, title, description, posted_by):
+    def __init__(self, title, description, posted_by=None, votes=None, flags=None):
         self.title = title
         self.description = description
         self.posted_by = posted_by
+        self.votes = votes
+        self.flags = flags
 
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String(140))
     commented_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    comment = db.relationship('User', backref=db.backref('comment',
+                              lazy='dynamic'))
 
-    def __init__(self, comment, commented_by):
+    def __init__(self, comment):
         self.comment = comment
-        self.commented_by = commented_by
+        #self.commented_by = commented_by
+
+if __name__ == '__main__':
+    manager.run()
