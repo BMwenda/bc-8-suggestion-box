975a2b1e389b1cb74ba87877e1cb14f44cb09e01
Boniface Mwenda <bonny90.m@gmail.com>
2016-06-23 15:38:04 +0300

    Added log in form

--- forms.py
+++ forms.py
@@ -1,16 +1,32 @@
-from wtforms import Form, StringField, PasswordField, validators
+from wtforms import StringField, PasswordField, validators, SubmitField
+from flask_wtf import Form
 
 
 class RegistrationForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [validators.DataRequired()])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    submit = SubmitField('Register')
+
+
+class LoginForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Enter Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    submit = SubmitField('Log In')
 
 
 class SuggestionForm(Form):
     title = StringField('Title', [validators.Length(min=4, max=50)])
     description = StringField(
         'Description', [validators.Length(min=6, max=140)])
+    submit = SubmitField('Suggest')
 
 
 class CommentForm(Form):
