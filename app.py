7c91935b3e41644909a00d9a39f95802dc1fc7a0
Boniface Mwenda <bonny90.m@gmail.com>
2016-07-08 16:54:33 +0300

    Added CSRF protection

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, request, render_template, redirect, flash, url_for
 from models import db, User, Suggestion, Comment
 from forms import RegistrationForm, SuggestionForm, CommentForm, LoginForm
 from flask_login import login_user, login_required, logout_user, current_user, LoginManager
+from flask_wtf.csrf import CsrfProtect
 
 app = Flask(__name__)
+# Load csrprotect
+CsrfProtect(app)
 # Create a secret key to prevent a CSRF attack
-app.secret_key = "some_hash_key"
+app.secret_key = "siri-kubwa"
+WTF_CSRF_SECRET_KEY = 'wtf i hate you'
+
 # Create a login_manager class that lets the app
 # and Flask-Login work together
 login_manager = LoginManager()
@@ -19,32 +24,40 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm(request.form)
-    if form.validate():
+    """This function logins in a user and creates a session"""
+    form = LoginForm()
+    if form.validate_on_submit():
+        # If validation is successful, create an instance of the user
         user = User.query.filter_by(username=form.username.data).first()
-        if user is not None:  # and user.decrypt_password(form.password):
-            login_user(user)
-            return redirect(url_for('add_suggestion'))
-        else:
-            flash("Invalid credentials! Not a user? Sign up first")
-            # return redirect(url_for('register'))
+        # Login user
+        login_user(user)
+        flash('You are now logged in')
+        next = request.args.get('next')
+        # next checks if user valid creds to
+        # access the next url
+        if not next:
+            flash("You don't have permission to access this page")
+            return redirect(url_for('home'))
+        return redirect(url_for('index'))
     return render_template('login.html', form=form)
-
-
-@app.route('/home')
-def home():
-    return render_template('index.html')
-
 
 @app.route('/logout')
 @login_required
 def logout():
-    user = current_user
-    user.authenticated = False
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for('base.html'))
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+
+@app.route('/index')
+@login_required
+def index():
+    return render_template('index.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -55,11 +68,12 @@ def register():
                     form.password.data)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('login'))
+        return redirect(url_for('index'))
     return render_template('register.html', form=form)
 
 
 @app.route('/suggest', methods=['GET', 'POST'])
+@login_required
 def add_suggestion():
     form = SuggestionForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -72,6 +86,7 @@ def add_suggestion():
 
 
 @app.route('/comment', methods=['GET', 'POST'])
+@login_required
 def add_comment():
     form = CommentForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -83,55 +98,51 @@ def add_comment():
 
 
 @app.route('/suggestions', methods=['GET', 'POST'])
+@login_required
 def suggestions():
-    # return str(Suggestion.query.all())
-    # return redirect(url_for('index.html'))
+    # query to view all suggestions by all users
     return render_template('view_all_suggestions.html')
 
 
 @app.route('/user_suggestions', methods=['GET', 'POST'])
+@login_required
 def user_suggestions():
-    # return str(Suggestion.query.all())
-    # return redirect(url_for('index.html'))
+    # query to view all suggestions made by the user
     return render_template('user_suggestions.html')
 
 
 @app.route('/comments', methods=['GET', 'POST'])
+@login_required
 def comments():
-    comments = User.query.all()
-    # return redirect(url_for('index.html'))
+    # Query to view all comments by all users
     return render_template('view_all_comments.html', Comment=comments)
 
 
 @app.route('/user_comments', methods=['GET', 'POST'])
+@login_required
 def user_comments():
-    # return str(Suggestion.query.all())
-    # return redirect(url_for('index.html'))
-    return render_template('view_user_comments.html')
+    # query to view all comments by the user
+    return render_template('user_comments.html')
 
-
+@login_required
 def delete_suggestion():
-    suggestion = Suggestion.query.get(id)
-    db.session.delete(suggestion)
-    db.session.commit()
-    flash("Suggestion deleted successfully")
-    return redirect(url_for("suggest"))
+    # method to delete user suggestion
+    return render_template('user_suggestions.html')
 
-
+@login_required
 def up_vote_suggestion():
-    v = Suggestion.query.first()
-    v.votes = Suggestion.votes + 1
-    flash("Record deleted!")
-    return redirect(url_for("suggest"))
+    # add method to implement up voting
+    return redirect(url_for('view_all_suggestions.html'))
 
-
+@login_required
 def down_vote_suggestion():
-    v = Suggestion.query.first()
-    if v.votes < 1:
-        pass
-    v.votes = Suggestion.votes - 1
-    flash("Record deleted!")
-    return redirect(url_for("suggest"))
+    # method to implement down voting
+    return render_template('view_all_suggestions.html')
+
+@login_required
+def flag():
+    # add a method to implement flagging comments
+    return render_template('view_all_comments.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
