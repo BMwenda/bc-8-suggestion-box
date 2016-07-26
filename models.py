47ce4ad2ff15a4feb61a8d94fc5f1ee082215355
Boniface Mwenda <bonny90.m@gmail.com>
2016-07-26 13:21:40 +0300

    Modified relationships in the records

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.login import UserMixin, LoginManager
+from flask.ext.login import UserMixin
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -16,66 +16,87 @@ manager.add_command('db', MigrateCommand)
 
 
 class User(db.Model, UserMixin):
+
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
 
-    def encrypt_password(self, password):
-        self.password_hash = generate_password_hash(password)
+    def set_password(self, pswd):
+        self.password = generate_password_hash(pswd)
 
-    def decrypt_password(self, password_hash):
-        return check_password_hash(self.password_hash, password_hash)
+    def check_password(self, pswd):
+        return check_password_hash(self.password, pswd)
 
     def is_active(self):
         return True
 
     def is_anonymous(self):
-        return True
+        return False
 
     def is_authenticated(self):
-        return self.authenticated
+        return True
 
     def get_id(self):
-        return (self.username)
+        return unicode(self.id)
 
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
-        self.password = password
+        self.set_password(password)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % (self.username)
 
 
 class Suggestion(db.Model):
+
+    __tablename__ = 'suggestions'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     description = db.Column(db.String(140))
-    posted_by = db.Column(db.Integer, db.ForeignKey('user.id'))
-    suggestion = db.relationship('User', backref=db.backref('user',
-                                                            lazy='dynamic'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship(
+        'User', backref=db.backref('suggestions', lazy='dynamic'))
     votes = db.Column(db.Integer)
     flags = db.Column(db.Integer)
 
-    def __init__(self, title, description, posted_by=None, votes=None, flags=None):
+    def __init__(self, title, description, user_id, votes=0, flags=0):
         self.title = title
         self.description = description
-        self.posted_by = posted_by
+        self.user_id = user_id
         self.votes = votes
         self.flags = flags
 
+    def __repr__(self):
+        return '<Suggestion %r>' % (self.title)
+
 
 class Comment(db.Model):
+
+    __tablename__ = 'comments'
+
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String(140))
-    commented_by = db.Column(db.Integer, db.ForeignKey('user.id'))
-    comm = db.relationship('User', backref=db.backref('comment',
-                                                      lazy='dynamic'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # Establish a relationship with 'users' table
+    user = db.relationship(
+        'User', backref=db.backref('comments', lazy='dynamic'))
+    suggestion_id = db.Column(db.Integer, db.ForeignKey('suggestions.id'))
+    # Relationship with 'suggestions' table
+    suggestion = db.relationship(
+        'Suggestion', backref=db.backref('comments', lazy='dynamic'))
 
-    def __init__(self, comment):
+    def __init__(self, comment, user_id):
         self.comment = comment
-        # self.commented_by = commented_by
+        self.user_id = user_id
+        # self.suggestion_id = suggestion_id
+
+    def __repr__(self):
+        return '<Comment %r>' % (self.comment)
 
 if __name__ == '__main__':
     manager.run()
